225052153f3449ff50ac93f017e024acc5223f1a
Jaz-3-0 <sebejaz99@gmail.com>
2024-03-14 07:52:13 +0200

    init configurations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,38 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const dotenv = require("dotenv");
+dotenv.config();
+
+function privateKey() {
+  return process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
+}
+
 module.exports = {
-  solidity: "0.8.24",
+  networks: {
+    mumbai: {
+      url: 'https://polygon-mumbai-pokt.nodies.app',
+      accounts: privateKey(),
+    },
+    sepolia: {
+      url: 'https://eth-sepolia.public.blastapi.io',
+      accounts: privateKey(),
+    },
+    arbitrum_sepolia: {
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      accounts: privateKey(),
+    }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      }
+    }
+  },
+  etherscan: {
+    apikey: process.env.API_KEY,
+  },
 };
